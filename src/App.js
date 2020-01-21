5ac04510d0d4fd78bad601a4b13d057eff1441ef
Seunghoooo <qkrtmdgh1207@naver.com>
2020-01-21 18:43:57 -0500

    Update App.js
    why did we stopped at this moment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets do this man
         </a>
       </header>
     </div>
