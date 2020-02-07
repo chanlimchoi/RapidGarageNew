6a26c48bd3a385294a4f2f6fc2804f7ef28b439d
chanlimchoi <choichanlim@gmail.com>
2020-02-07 01:04:51 -0500

    post table

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import Post from './Post';
 import Map from './Map';
+import '../App.css';
 
 function resultList(props){
       return (
-        
-    <div className="panel-info">
-      <div className="panel-heading">
-      <span><h4>{props.title} </h4><span className="glyphicon glyphicon-time"></span> Post by {props.userId}, post on {props.updatedAt} </span>
-      </div>
-      <div className="panel-body">
-        <h5><span className="label label-danger">Devices</span> <span className="label label-primary">Furniture</span></h5>
-        Location: {props.street}, {props.city}, {props.state} <br/>
-        <h5>start on {props.startDate} end on {props.endDate}</h5>
-      </div>
-    </div>
-    
+     
+
+
+  
+      <td><h4>{props.title} </h4> Post by {props.userId}, post on {props.updatedAt} <h5><span className="label label-danger">Devices</span> <span className="label label-primary">Furniture</span></h5>
+        Location: {props.street}, {props.city}, {props.state} <br/>  <h5>start on {props.startDate} end on {props.endDate}</h5></td>
+     
+  
   );
 }
 
@@ -88,20 +85,23 @@ console.log("search value: " + val);
             <input type="submit" value="Search" />
           </ form>
           <div className="panel-group">
-            <h5><small>RECENT POSTS</small></h5>
+            
+            <table id="garages">
+                <th><h5><small>RECENT POSTS</small></h5></th>
               {
           (this.state.searchResult) ?
           (this.state.searchResult.map((post,key)=>{
             return (
-              <li key={key}>
+              <tr key={key}>
                 
   { (resultList(post))} 
-              </li>
+              </tr>
 
             )
           })
           ): <h1>No data</h1>
         }
+        </table>
 
            
           </div>
